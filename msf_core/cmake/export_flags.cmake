d9b7e693ff5fe297ec8a37327b65ed2335b59ac7
chengshy <chengshangyi@gmail.com>
2017-02-13 15:15:53 -0800

    Fix compile double free

--- msf_core/cmake/export_flags.cmake
+++ msf_core/cmake/export_flags.cmake
@@ -8,7 +8,5 @@ SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wno-unused-parameter")
 # assembler on mac os doesn't know avx commands :( switch to sse4.2. assuming that our mac machines have a corei7
 IF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin") 
   SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse4.2")
-ELSE(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=native")
 ENDIF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 
